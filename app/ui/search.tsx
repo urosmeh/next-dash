fd5ad140020693d3d97ca9638b32864b5553c3d5
urosmeh <uros.meh@gmail.com>
2024-06-13 12:37:59 +0200

    query params, search, usedebounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
+const PARAMS_KEY = 'query';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set(PARAMS_KEY, value);
+    } else {
+      params.delete(PARAMS_KEY);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 250);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +29,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => handleSearch(event.target.value)}
+        defaultValue={searchParams.get(PARAMS_KEY)?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
